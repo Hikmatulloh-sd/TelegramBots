321bc01ea44091f2f6c1c69274042bc4c135e694
Hikmatulloh-sd <hikmatulloh.sd@gmail.com>
2025-03-13 11:43:18 +0600

    startup shutdown

--- config.py
+++
@@ -1,6 +0,0 @@
-import os 
-from dotenv import load_dotenv
-
-load_dotenv
-
-BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
--- echo_bot/echo_bot.py
+++ echo_bot/echo_bot.py
@@ -1,19 +1,34 @@
+import os
 import asyncio
+
 from aiogram import Bot, Dispatcher, types
-from .. import config
+from dotenv import load_dotenv
+
+load_dotenv()
 
 dp = Dispatcher()
-bot = Bot(config.BOT_TOKEN)
+bot = Bot(os.getenv('TELEGRAM_BOT_TOKEN'))
 
 
-async def echo(message: types.Message,bot: Bot):
+async def startup_answer(bot: Bot):
+    await bot.send_message(chat_id=5507893747, text="Bot ishga tushti!")
+
+
+async def shutdown_answer(bot: Bot):
+    await bot.send_message(chat_id=5507893747, text="Bot tohtadi!!!")
+
+
+async def echo(message: types.Message, bot: Bot):
     await message.copy_to(chat_id=message.chat.id)
 
     
 async def main():
+    dp.startup.register(startup_answer)
+    dp.shutdown.register(shutdown_answer)
     dp.message.register(echo)
 
     await dp.start_polling(bot)
 
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
